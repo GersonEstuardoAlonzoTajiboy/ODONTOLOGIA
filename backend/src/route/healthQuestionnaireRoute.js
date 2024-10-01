111e133a386b9e435217a4694234811fecd1508d
GEAT <111700508+GersonEstuardoAlonzoTajiboy@users.noreply.github.com>
2024-09-30 18:29:14 -0600

    UPDATE
    UPDATE

--- backend/src/route/healthQuestionnaireRoute.js
+++ backend/src/route/healthQuestionnaireRoute.js
@@ -1,16 +1,10 @@
 import express from 'express';
 import verifyToken from '../middleware/verifyToken.js';
-import { registerHealthQuestionnaire, updateHealthQuestionnaire, findHealthQuestionnaireByPatientId } from '../controller/HealthQuestionnaireController.js';
+import { registerHealthQuestionnaire } from '../controller/HealthQuestionnaireController.js';
 
 const router = express.Router();
 
 // ENDPOINT - REGISTER HEALTH QUESTIONNARIE
 router.post('/health-questionnarie', verifyToken, registerHealthQuestionnaire);
-
-// ENDPOINT - UPDATE HEALTH QUESTIONNARIE
-router.put('/health-questionnarie/:id', verifyToken, updateHealthQuestionnaire);
-
-// ENDPOIN - FIND ONE HEALTH QUESTIONNARIE
-router.get('/health-questionnarie/:patient_id', verifyToken, findHealthQuestionnaireByPatientId);
 
 export default router; 
