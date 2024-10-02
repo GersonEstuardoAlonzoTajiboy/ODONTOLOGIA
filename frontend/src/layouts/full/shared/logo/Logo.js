ab9f1ae21684c0f319a78d67df1e78321d926673
GEAT <111700508+GersonEstuardoAlonzoTajiboy@users.noreply.github.com>
2024-10-01 18:54:09 -0600

    UPDATE
    UPDATE

--- frontend/src/layouts/full/shared/logo/Logo.js
+++ frontend/src/layouts/full/shared/logo/Logo.js
@@ -3,8 +3,8 @@ import LogoDark from '../../../../assets/images/logos/IMG-20240709-WA0006.jpg';
 import { styled } from '@mui/material';
 
 const LinkStyled = styled(Link)(() => ({
-  height: '40px',
-  width: '150px',
+  height: '50px',
+  width: '220px',
   overflow: 'hidden',
   display: 'block',
 }));
@@ -12,7 +12,7 @@ const LinkStyled = styled(Link)(() => ({
 const Logo = () => {
   return (
     <LinkStyled>
-      <img src={LogoDark} alt="Logo" height={40} width={150} />
+      <img src={LogoDark} alt="Logo" height={50} width={220} />
     </LinkStyled>
   );
 };
